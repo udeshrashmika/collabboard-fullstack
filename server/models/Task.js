3569e361846598497c63c7744da41b19072bb2a4
Udesh <apurdesilva@students.nsbm.ac.lk>
2026-08-27 10:45:33 +0530

    refactor(task): use shared database task model

--- server/models/Task.js
+++ server/models/Task.js
@@ -1,21 +1,31 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const taskSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  description: { type: String },
-  columnId: { 
-    type: mongoose.Schema.Types.ObjectId, 
-    ref: 'Column', 
-    required: true 
+const taskSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+
+    description: {
+      type: String,
+    },
+
+    columnId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Column",
+      required: true,
+    },
+
+    assignee: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
   },
-  assignee: { 
-    type: mongoose.Schema.Types.ObjectId, 
-    ref: 'User' 
+  {
+    timestamps: true,
+    optimisticConcurrency: true,
   }
-}, { 
-  timestamps: true,
-  optimisticConcurrency: true 
-});
+);
 
-export default mongoose.model('Task', taskSchema);
-
+export default mongoose.model("Task", taskSchema);
